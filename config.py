fec273c7bd8837adf7c97b45f0eee7ff2eb6cab4
Tanich28 <tatizinkevich@gmail.com>
2021-12-15 22:37:05 +0300

    Bot

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "1237217182:AAFGbtocD9w_nsfM2cwIdwyO6PYJj8FgVCw"
+APP_URL = "https://tanichbot.herokuapp.com/" + BOT_TOKEN
+DB_URI = "postgres://puituzgmngqiwh:9b4f079c0766513bd92150923a63bb17c1d3505460b059396fcb8d0599757aa2@ec2-54-78-211-131.eu-west-1.compute.amazonaws.com:5432/dcrqes2g6amhmi"
