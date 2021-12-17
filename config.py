4dbb55f9005903f5f595418ee33dcec4d3f27b77
Tanich28 <tatizinkevich@gmail.com>
2021-12-17 23:48:50 +0300

    postgres

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "1237217182:AAFGbtocD9w_nsfM2cwIdwyO6PYJj8FgVCw"
 APP_URL = "https://tanichbot.herokuapp.com/" + BOT_TOKEN
-DB_URI = "postgres://puituzgmngqiwh:9b4f079c0766513bd92150923a63bb17c1d3505460b059396fcb8d0599757aa2@ec2-54-78-211-131.eu-west-1.compute.amazonaws.com:5432/dcrqes2g6amhmi"
+DB_URI = "postgres://vefdgdmwtomnis:54987eafbe578612b5c1a14ec55cf80d83a49ffdce0c2e6003a989f76432898d@ec2-34-253-116-145.eu-west-1.compute.amazonaws.com:5432/df21k81cktpk91"
--- main.py
+++ main.py
@@ -10,13 +10,13 @@ server = Flask(__name__)
 logger = telebot.logger
 logger.setLevel(logging.DEBUG)
 
-# db_connection = psycopg2.connect(DB_URI, sslmode="require")
-# db_object = db_connection.cursor()
+db_connection = psycopg2.connect(DB_URI, sslmode="require")
+db_object = db_connection.cursor()
 
 
-# def update_messages_count(user_id):
-#     db_object.execute(f"UPDATE users SET messages = messages + 1 WHERE id = {user_id}")
-#     db_connection.commit()
+def update_messages_count(user_id):
+    db_object.execute(f"UPDATE users SET messages = messages + 1 WHERE id = {user_id}")
+    db_connection.commit()
 
 
 @bot.message_handler(commands=["start"])
@@ -25,36 +25,36 @@ def start(message):
     username = message.from_user.username
     bot.reply_to(message, f"Hello, {username}!")
 
-    # db_object.execute(f"SELECT id FROM users WHERE id = {user_id}")
-    # result = db_object.fetchone()
+    db_object.execute(f"SELECT id FROM users WHERE id = {user_id}")
+    result = db_object.fetchone()
 
-#     if not result:
-#         db_object.execute("INSERT INTO users(id, username, messages) VALUES (%s, %s, %s)", (user_id, username, 0))
-#         db_connection.commit()
-#
-#     update_messages_count(user_id)
-#
-#
-# @bot.message_handler(commands=["stats"])
-# def get_stats(message):
-#     db_object.execute("SELECT * FROM users ORDER BY messages DESC LIMIT 10")
-#     result = db_object.fetchall()
-#
-#     if not result:
-#         bot.reply_to(message, "No data...")
-#     else:
-#         reply_message = "- Top flooders:\n"
-#         for i, item in enumerate(result):
-#             reply_message += f"[{i + 1}] {item[1].strip()} ({item[0]}) : {item[2]} messages.\n"
-#         bot.reply_to(message, reply_message)
-#
-#     update_messages_count(message.from_user.id)
-#
-#
-# @bot.message_handler(func=lambda message: True, content_types=["text"])
-# def message_from_user(message):
-#     user_id = message.from_user.id
-#     update_messages_count(user_id)
+    if not result:
+        db_object.execute("INSERT INTO users(id, username, messages) VALUES (%s, %s, %s)", (user_id, username, 0))
+        db_connection.commit()
+
+    update_messages_count(user_id)
+
+
+@bot.message_handler(commands=["stats"])
+def get_stats(message):
+    db_object.execute("SELECT * FROM users ORDER BY messages DESC LIMIT 10")
+    result = db_object.fetchall()
+
+    if not result:
+        bot.reply_to(message, "No data...")
+    else:
+        reply_message = "- Top flooders:\n"
+        for i, item in enumerate(result):
+            reply_message += f"[{i + 1}] {item[1].strip()} ({item[0]}) : {item[2]} messages.\n"
+        bot.reply_to(message, reply_message)
+
+    update_messages_count(message.from_user.id)
+
+
+@bot.message_handler(func=lambda message: True, content_types=["text"])
+def message_from_user(message):
+    user_id = message.from_user.id
+    update_messages_count(user_id)
 
 
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
